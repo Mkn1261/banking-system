c8ebe77f5ac2d3fb4b71c79c278b7873fc196377
Mkn1261 <nishadmahesh1803@gmail.com>
2026-08-10 23:15:20 +0530

    Add TransferRequest,TransferResponse file

--- transaction-service/src/main/java/com/banking/transactionservice/entity/Transaction.java
+++ transaction-service/src/main/java/com/banking/transactionservice/entity/Transaction.java
@@ -4,8 +4,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "transaction")
@@ -16,14 +18,30 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(nullable = false)
     private String senderAccountNumber;
 
+    @Column(nullable = false, precision = 15, scale = 2)
     private String receiverAccountNumber;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private BigDecimal amount;
 
     private TransactionType type;
 
     private TransactionStatus status;
+
+    private String description;
+
+    private String failureReason;
+
+    private String referenceNumber;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private LocalDateTime completedAt;
 
 }
--- transaction-service/src/main/java/com/banking/transactionservice/entity/TransactionStatus.java
+++ transaction-service/src/main/java/com/banking/transactionservice/entity/TransactionStatus.java
@@ -1,4 +1,23 @@
 package com.banking.transactionservice.entity;
 
+
+/**
+ * Transaction life cycle flow
+ *
+ * PENDING -> PROCESSING -> COMPLETED (clean transaction)
+ *                       -> PENDING_VERIFICATION(suspicious detected)
+ *                                  ->COMPLETED(verified)
+ *                                  ->FLAGED (SAGA REFUND)
+ *                       ->FAILED
+ *                       ->FLAGED
+ *
+ */
 public enum TransactionStatus {
+
+    PENDING,
+    PROCESSING,
+    PENDING_VERIFICATION,
+    COMPLETED,
+    FAILED,
+    FLAGGED
 }
