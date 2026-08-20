6fe36a0a65371bbfc5f3959d48bb861f0e15164b
Mkn1261 <nishadmahesh1803@gmail.com>
2026-08-20 16:32:32 +0530

    Add NotificationService fime in notification-service

--- transaction-service/src/main/java/com/banking/transactionservice/client/AccountServiceClient.java
+++ transaction-service/src/main/java/com/banking/transactionservice/client/AccountServiceClient.java
@@ -1,12 +1,14 @@
 package com.banking.transactionservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
 
+@Component
 @FeignClient(name = "account-service", url = "&{account.service.url}")
 public interface AccountServiceClient {
 
