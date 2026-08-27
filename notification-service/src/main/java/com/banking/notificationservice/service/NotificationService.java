935a18f793de935f9e6f5886e5af247cdb4b6069
Mkn1261 <nishadmahesh1803@gmail.com>
2026-08-28 02:33:33 +0530

    Consume all the Events

--- notification-service/src/main/java/com/banking/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/com/banking/notificationservice/service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.banking.notificationservice.service;
 
 
+import jakarta.mail.search.SearchTerm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -41,6 +42,116 @@ public class NotificationService {
         }
     }
 
+    @KafkaListener(topics = "transaction.completed")
+    public void consumeTransactionCompleted(
+            @Payload Map<String, Object> payload){
+        try {
+
+            String senderAccount = (String) payload.get("senderAccountNumber");
+            String receiverAccount = (String) payload.get("receiverAccountNumber");
+            String amount = payload.get("amount").toString();
+
+            // DEBIT ALERT
+            sendAlert(senderAccount,
+                    "DEBIT ALERT",
+                    String.format(
+                            "%s debited from account %s",
+                            amount, senderAccount
+                    ));
+
+            // CREDIT ALERT
+            sendAlert(receiverAccount,
+                    "CREDIT ALERT",
+                    String.format(
+                            "%s credited from account %s",
+                            amount, receiverAccount
+                    ));
+        } catch (Exception e) {
+            log.error("Error sending transaction notification: {}", e.getMessage());
+        }
+    }
+
+    @KafkaListener(topics = "fraud.detected")
+    public void consumeFraudDetected(
+            @Payload Map<String, Object> payload){
+        try {
+
+            String accountNumber = (String) payload.get("accountNumber");
+            String reason = (String) payload.get("reason");
+
+            sendAlert(accountNumber,
+                    "SUSPICIOUS ACTIVITY DETECTED",
+                    String.format(
+                            "Reason: %s. "+
+                                    "Please contact your bank immediatly.",
+                            accountNumber, reason
+                    ));
+        } catch (Exception e) {
+            log.error("Error sending fraud alert: {}", e.getMessage());
+        }
+    }
+
+    @KafkaListener(topics = "transaction.refunded")
+    public void consumeTransactionRefunded(
+            @Payload Map<String, Object> payload){
+        try {
+
+            String senderAccount = (String) payload.get("senderAccountNumber");
+            String amount = payload.get("amount").toString();
+            String reason = (String) payload.get("reason");
+
+            sendAlert(senderAccount,
+                    "REFUND PROCESSED",
+                    String.format(
+                            "Your transaction of %s was cancelled "+
+                                    "Reason: %s "+
+                                    "%s has been refunded to account %s. ",
+                            amount, reason, amount, senderAccount
+                    ));
+        } catch (Exception e) {
+            log.error("Error sendinf refund notification: {}", e.getMessage());
+        }
+    }
+
+    @KafkaListener(topics = "payment.completed")
+    public void consumePaymentCompleted(
+            @Payload Map<String, Object> payload){
+        try {
+            String accountNumber = (String) payload.get("accountNumber");
+            String amount = payload.get("amount").toString();
+
+            sendAlert(accountNumber,
+                    "PAYMENT SUCCESSFUL",
+                    String.format(
+                            "Payment of %s completed. "+
+                                    "Razorpay ID: %s",
+                            amount, payload.get("razorpayPaymentId")
+                    ));
+        } catch (Exception e) {
+            log.error("Error sending payment notification: {}", e.getMessage());
+        }
+    }
+
+    @KafkaListener(topics = "payment.failed")
+    public void consumePaymentFailed(
+            @Payload Map<String, Object> payload){
+        try {
+
+            String accountNumber = (String) payload.get("accountNumber");
+            String amount = payload.get("amount").toString();
+
+            sendAlert(accountNumber,
+                    "PAYMENT FAILED",
+                    String.format(
+                            "Your payment of %s could not be processed. "+
+                                    "Please tr again or contact support.",
+                            amount
+                    ));
+
+        } catch (Exception e) {
+            log.error("Error sending payment failure notification: {}", e.getMessage());
+        }
+    }
     private void sendAlert(String accountNumber, String subject, String message){
 
     }
