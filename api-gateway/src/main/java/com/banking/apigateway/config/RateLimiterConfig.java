6399609eb1785b1c53b031c89411a2286893b36a
Mkn1261 <nishadmahesh1803@gmail.com>
2026-09-06 15:38:57 +0530

    fixing some error

--- api-gateway/src/main/java/com/banking/apigateway/config/RateLimiterConfig.java
+++ api-gateway/src/main/java/com/banking/apigateway/config/RateLimiterConfig.java
@@ -1,16 +1,16 @@
 package com.banking.apigateway.config;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Configuration
 public class RateLimiterConfig {
 
     @Bean
-    public keyResolver keyResolver() {
-        return ServerWebExchange exchange -> Mono.just(
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just(
                 exchange.getRequest()
                         .getRemoteAddress()
                         .getAddress()
